dccc7e163c86dc612495f8b7aeca5b103b9f0cab
sabari-35 <sabari.psmail@gmail.com>
2026-03-05 06:51:02 +0530

    fixed eslint warnings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { Rnd } from 'react-rnd';
 import axios from 'axios';
 import './index.css';
 
-const API_BASE = process.env.REACT_APP_API_URL || 'http://localhost:8000/api';
+const API_BASE = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
 
 interface TextElement {
   id: string;
